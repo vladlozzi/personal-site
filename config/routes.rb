0b3c8b065c40e68d5bc22eb390ab9c0a0e69f18f
Vlad Lozynskyi <vladloz@hotmail.com>
2025-03-30 13:24:30 +0300

    Delete the LinkedIn reference

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  if Rails.env.test?
+    namespace :test do
+      resource :session, only: %i[create]
+    end
+  end
   resources :projects
   root to: "home#index"
   get "/auth/:provider/callback", to: 'sessions#create'
   get "/auth/exit", to: 'sessions#destroy'
   get "/contacts", to: 'contacts#index'
   get "/facebook", to: redirect("https://www.facebook.com/vladloz/")
-  get "/linkedin", to: redirect("https://www.linkedin.com/in/vlad-lozynskyi-756a324a/")
   get "/telegram", to: redirect("https://t.me/vladloz")
   get "/github", to: redirect("https://github.com/vladlozzi")
 end
--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Contacts", type: :request do
-  describe "GET /index" do
+  describe "GET /contacts" do
     it "returns http success" do
-      get "/contacts/index"
+      get "/contacts"
       expect(response).to have_http_status(:success)
     end
   end
